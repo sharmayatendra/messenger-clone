fb1918f09cdb56854b7240a9a5cbbe0fed6c52d4
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2021-05-11 23:59:15 +0530

    message update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import {  FormControl, Input } from '@material-ui/core';
-import React , { useEffect, useState } from 'react'
+// import {  FormControl } from '@material-ui/core';
+import React , { useEffect, useRef, useState } from 'react'
 import './App.css';
 import db from './firebase';
 import Message from './Message';
 import firebase from 'firebase';
 import FlipMove from 'react-flip-move';
 import SendIcon from '@material-ui/icons/Send';
-import { IconButton } from '@material-ui/core';
+// import { IconButton } from '@material-ui/core';
 
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
 
   useEffect(() => {
     db.collection('messages')
-    .orderBy('timestamp','desc')
+    .orderBy('timestamp','asc')
     .onSnapshot(snapshot => {
       setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})))
     })
@@ -28,48 +28,56 @@ function App() {
     setUsername(prompt("please enter your name"))
   }, [])
   
+  const dummy = useRef();
   const sendMessage = (event) => {
-    event.preventDefault();                    //prevents refreshing of the page
+    event.preventDefault();
     db.collection('messages').add({
       message: input,
       username: username,
       timestamp: firebase.firestore.FieldValue.serverTimestamp()
     })
+    dummy.current.scrollIntoView({ behavior: "smooth" });
 
 
     setMessages([...messages, {username: username, message: input}]);         //spread operator use to append the input to existing msg list
     setInput('');                              //clears input field after msg sent
     
-
+    
   }
   return (
     <div className="App">
-      
+
+      <header>
       <h1>ChIt-ChAt📧</h1>
-      <h2>welcome {username} </h2>
+      <h2>{username} </h2>
+      </header>
+      <section>
+      
       
       <form className="app__form">
-      <FormControl className="app__formControl">
+      {/* <FormControl className="app__formControl"> */}
         {/* <InputLabel>Enter a message</InputLabel> */}
-        <Input className="app__input" placeholder={"enter a message"} value={input} onChange={event => setInput(event.target.value)} />
+        <input className="app__input" placeholder={"enter a message"} value={input} onChange={event => setInput(event.target.value)} />
 
-        <IconButton className="app__iconButton" disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>
+        <button className="app__iconButton" disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>
 
           <SendIcon></SendIcon>
-        </IconButton>
+        </button>
         {/* <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button> */}
-      </FormControl>
-      
-      
+      {/* </FormControl> */}
       
       </form>
-
-      <FlipMove>
+      
+      <main>
+      {/* <FlipMove> */}
         {messages.map(({ id, message }) => (
           <Message key={id} username={username} message={message} />
         ))}
-      </FlipMove>
-        
+        <span ref={dummy}></span>
+      {/* </FlipMove> */}
+      
+      </main>
+      </section>
     </div>
   );
 }
